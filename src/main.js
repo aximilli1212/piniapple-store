d70dfb4e24609efeee0da413c4befc6a0ffbf505
Emmanuel Sackey <aximilli1212@gmail.com>
2023-11-05 20:54:54 +0000

    Add pinia custom plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import {createPinia} from "pinia";
+import { createPinia } from "pinia";
 
 // Icons and Styles
 import FontAwesomePlugin from "./plugins/FontAwesome";
@@ -10,12 +10,15 @@ import "./assets/main.pcss";
 import AppButton from "./components/AppButton.vue";
 import AppCountInput from "./components/AppCountInput.vue";
 import AppModalOverlay from "./components/AppModalOverlay.vue";
+import { PiniaHistoryPlugin } from "@/plugins/PiniaHistoryPlugin";
 
+const pinia = createPinia();
+pinia.use(PiniaHistoryPlugin);
 // Init App
 createApp(App)
-  .use(createPinia())
-  .use(FontAwesomePlugin)
-  .component("AppButton", AppButton)
-  .component("AppCountInput", AppCountInput)
-  .component("AppModalOverlay", AppModalOverlay)
-  .mount("#app");
+    .use(pinia)
+    .use(FontAwesomePlugin)
+    .component("AppButton", AppButton)
+    .component("AppCountInput", AppCountInput)
+    .component("AppModalOverlay", AppModalOverlay)
+    .mount("#app");
