dbeec22012a6d1c82f326d91bb13cc021642c135
Emmanuel Sackey <aximilli1212@gmail.com>
2023-11-05 19:02:06 +0000

    fix product store fetch not working

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import {useProductStore} from "@/stores/ProductStore";
 
-const {products, fetchProducts} = useProductStore();
-fetchProducts()
+const productStore = useProductStore();
 
+productStore.fetchProducts()
 </script>
 
 <template>
@@ -13,7 +13,7 @@ fetchProducts()
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
